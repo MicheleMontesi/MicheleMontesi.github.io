12dedb13dcbd83683a035ca0053c25e4140cd933
Michele Montesi <michelemontesi36@gmail.com>
2023-09-24 15:12:47 +0200

    Update in progress

--- scripts/bubblesScript.js
+++ scripts/bubblesScript.js
@@ -1,72 +1,42 @@
-const bubbles = document.querySelectorAll('.bubble');
+// Seleziona tutti i bottoni per aprire i modali
+var openModalBtns = document.querySelectorAll('.bubble');
+console.log(openModalBtns);
 
-// Funzione per chiudere una bolla
-function closeBubble(bubble) {
-  if (bubble.classList.contains('enlarged')) {
-    bubble.classList.remove('enlarged');
-    const h1 = bubble.querySelector('.centered-content h1');
-    const h2Elements = bubble.querySelectorAll('.centered-content h2');
-    h1.style.display = 'block';
-    h2Elements.forEach(h2 => {
-      h2.style.display = "none";
+// Aggiungi un gestore di eventi a ciascun bottone
+openModalBtns.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        // Ottieni l'ID del modal associato a questo bottone
+        var modalId = this.getAttribute('id').replace('openM', 'm');
+        var modal = document.getElementById(modalId);
+        modal.style.display = 'flex';
     });
-  }
+});
+
+// Funzione per chiudere il modal
+function closeModal(modalId) {
+    var modal = document.getElementById(modalId);
+    modal.style.display = 'none';
 }
 
-bubbles.forEach((bubble) => {
-  const h1 = bubble.querySelector('.centered-content h1');
-  const h2Elements = bubble.querySelectorAll('.centered-content h2');
+// Chiudi il modal quando si fa clic sulla "X" per ciascun modal
+var closeBtns = document.querySelectorAll('.close');
+closeBtns.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        var modalId = this.classList[1].replace('close', 'modal');
+        closeModal(modalId);
+    });
+});
 
-  h2Elements.forEach(h2 => {
-    h2.style.display = "none";
-  });
-  const randomDuration = Math.random() * (15 - 4) + 4; // Calcola una durata casuale tra 4 e 15 secondi
-  bubble.style.animationDuration = `${randomDuration}s`
-
-  bubble.addEventListener('click', () => {
-    if (!bubble.classList.contains('enlarged')) {
-      // Chiudi tutte le bolle aperte prima di aprire questa
-      bubbles.forEach((otherBubble) => {
-        if (otherBubble !== bubble && otherBubble.classList.contains('enlarged')) {
-          closeBubble(otherBubble);
+var modalContainers = document.querySelectorAll('.modal-container');
+var bubbles = document.querySelectorAll('.bubble');
+modalContainers.forEach(function (container) {
+  bubbles.foreach(function (bubble) {
+    container.addEventListener('click', function (event) {
+        if (event.target !== this && event.target !== bubble) {
+            var modalId = this.firstElementChild.getAttribute('id');
+            closeModal(modalId);
         }
-      });
-    } 
-
-    bubble.classList.toggle("enlarged");
-
-    if (bubble.classList.contains('enlarged')) {
-      h1.style.display = 'none';
-      h2Elements.forEach(h2 => {
-        h2.style.display = "block";
-      });
-    } else {
-      h1.style.display = 'block';
-      h2Elements.forEach(h2 => {
-        h2.style.display = "none";
-      });
-    }
+    });
   })
-})
-
-// Crea un observer che osserva tutte le sezioni.
-const sectionObserver = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (!entry.isIntersecting || entry.target.id !== 'skills') {
-      // Se l'utente esce dalla sezione "skills", chiudi tutte le bolle.
-      bubbles.forEach((bubble) => {
-        closeBubble(bubble);
-      });
-    }
-  });
-}, {
-  root: null,
-  rootMargin: '0px',
-  threshold: 0,
 });
 
-// Osserva tutte le sezioni.
-const sections = document.querySelectorAll('.page');
-sections.forEach((section) => {
-  sectionObserver.observe(section);
-});
