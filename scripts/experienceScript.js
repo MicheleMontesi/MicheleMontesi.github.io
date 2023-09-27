4e1c3039c07773792b918386076d786f4fa81d8b
Michele Montesi <michelemontesi36@gmail.com>
2023-09-27 20:08:10 +0200

    Added eng language for experiences

--- scripts/experienceScript.js
+++ scripts/experienceScript.js
@@ -1,7 +1,7 @@
 const galleryView = document.getElementById('galleryView')
 galleryView.style.display = 'flex'
 
-const imgObject = [
+const itaExperiences = [
   {
     title: 'Tirocinante Programmatore',
     startDate: '05/06/2023',
@@ -21,7 +21,7 @@ const imgObject = [
     title:
       'Insegnante Di Ripetizioni Di Materie Informatiche, Matematiche E Scientifiche',
     startDate: '01/03/2023',
-    endDate: 'Today',
+    endDate: 'In corso',
     jobLocation: 'Remoto / Da casa',
     descriptionList: [
       'Ripetizioni di C, Python e Informatica di base',
@@ -91,9 +91,111 @@ const imgObject = [
   },
 ]
 
-let mainImg = 0
-let prevImg = imgObject.length - 1
-let nextImg = 1
+const engExperiences = [
+  {
+    title: 'Intern Programmer',
+    startDate: '06/05/2023',
+    endDate: '09/05/2023',
+    jobLocation: 'University of Bologna - Cesena Campus',
+    descriptionList: [
+      'Creation of Ansible scripts',
+      'Study and use of Docker Swarm',
+      "Study and use of Nginx and Let's Encrypt",
+      'Study and use of FreeIPA',
+      'Study on the usage of Teleport',
+      'Study and use of Active Directory',
+      'Integration of Radius authentication in Teleport',
+    ],
+  },
+  {
+    title: 'Tutor for Computer Science, Mathematics, and Sciences',
+    startDate: '03/01/2023',
+    endDate: 'In progress',
+    jobLocation: 'Remote / Work from Home',
+    descriptionList: [
+      'Tutoring in C, Python, and Basic Computer Science',
+      'Tutoring in Mathematics (1st year of high school)',
+      'Tutoring in Biology and Chemistry',
+    ],
+  },
+  {
+    title: 'Appetizer Preparation Assistant',
+    startDate: '06/14/2020',
+    endDate: '07/15/2020',
+    jobLocation: 'Locanda da Lorenzo - Cervia',
+    descriptionList: [
+      'Preparation of various appetizers',
+      'Assisting the chef',
+      'Assisting with grilling',
+      'Sanitizing tools and counters',
+    ],
+  },
+  {
+    title: 'Volunteer Associate',
+    startDate: '10/15/2019',
+    endDate: '03/15/2020',
+    jobLocation: 'L\'Oasi - Cesena',
+    descriptionList: [
+      'Maintenance of Windows and Mac computers (Hard Disk cloning with subsequent replacement, RAM replacement, Computer optimization as per privacy law)',
+      "Creation of the cooperative's website",
+      'On-site and remote assistance for problem resolution with hardware and software usage',
+      'Installation and management of software (Microsoft Office, Antivirus, TeamViewer)',
+      'Autonomous work management based on required objectives',
+    ],
+  },
+  {
+    title: 'Volunteer Associate',
+    startDate: '07/01/2016',
+    endDate: '01/31/2018',
+    jobLocation: 'L\'Oasi - Cesena',
+    descriptionList: [
+      "Creation of the cooperative's website",
+      'On-site and remote assistance for problem resolution with hardware and software usage',
+      'Installation and management of software (Microsoft Office, Antivirus)',
+    ],
+  },
+  {
+    title: 'Intern',
+    startDate: '01/15/2018',
+    endDate: '02/03/2018',
+    jobLocation: 'Gestiass - Cesena',
+    descriptionList: [
+      'Creation of Excel files',
+      'IT support',
+      'Claims management',
+      'Usage of company platforms',
+      'Phone and in-person interactions with clients',
+    ],
+  },
+  {
+    title: 'Intern',
+    startDate: '05/22/2017',
+    endDate: '06/17/2017',
+    jobLocation: 'Auto Sat S.P.A - Cesena',
+    descriptionList: [
+      'IT support',
+      'Inventory management',
+      'Creation of graphics',
+    ],
+  },
+]
+
+const languageSelectorExp = document.getElementById('language')
+let currentLanguage = languageSelectorExp.value
+
+document.getElementById('language').addEventListener('change', onLanguageChange)
+
+let experiences = currentLanguage === 'ita' ? itaExperiences : engExperiences
+
+function onLanguageChange() {
+  currentLanguage = languageSelectorExp.value
+  experiences = currentLanguage === 'ita' ? itaExperiences : engExperiences
+  loadGallery()
+}
+
+let mainExp = 0
+let prevExp = experiences.length - 1
+let nextExp = 1
 
 let isAtLeftEnd = true
 let isAtRightEnd = false
@@ -103,20 +205,35 @@ function loadGallery() {
   const leftView = document.getElementById('leftView')
   const rightView = document.getElementById('rightView')
 
-  const currentImg = imgObject[mainImg]
+  const currentExp = experiences[mainExp]
 
-  const descriptionList = currentImg.descriptionList
+  const descriptionList = currentExp.descriptionList
     .map((item) => `<li>${item}</li>`)
     .join('')
 
+  function translate(text) {
+    const translations = {
+      "Data d'inizio:": 'Start Date:',
+      'Data di fine:': 'End Date:',
+      'Luogo di lavoro:': 'Job Location:',
+    }
+    return currentLanguage === 'ita' ? text : translations[text] || text
+  }
+
   mainView.innerHTML = `
-    <h2>${currentImg.title}</h2>
+    <h2>${currentExp.title}</h2>
     <div class="job-date-location-container">
       <div class="job-date-container">
-        <p class="job-date">Data d'inizio:<br/>${currentImg.startDate}</p>
-        <p class="job-date">Data di fine:<br/>${currentImg.endDate}</p>
+        <p class="job-date">${translate("Data d'inizio:")}<br/>${
+          currentExp.startDate
+        }</p>
+        <p class="job-date">${translate('Data di fine:')}<br/>${
+          currentExp.endDate
+        }</p>
       </div>
-      <p class="job-location">Luogo di lavoro:<br/>${currentImg.jobLocation}</p>
+      <p class="job-location">${translate('Luogo di lavoro:')}<br/>${
+        currentExp.jobLocation
+      }</p>
     </div>
     <div class="job-description-box">
       <ul class="job-description">${descriptionList}</ul>
@@ -124,11 +241,11 @@ function loadGallery() {
   `
   // Aggiorna il testo delle descrizioni nel carosello
   leftView.innerHTML = `
-    <h2>${imgObject[prevImg].title}</h2>
+    <h2>${experiences[prevExp].title}</h2>
   `
 
   rightView.innerHTML = `
-    <h2>${imgObject[nextImg].title}</h2>
+    <h2>${experiences[nextExp].title}</h2>
   `
 
   if (isAtLeftEnd) {
@@ -150,13 +267,13 @@ function loadGallery() {
 
 function scrollRight() {
   if (!isAtRightEnd) {
-    prevImg = mainImg
-    mainImg = nextImg
-    if (nextImg >= imgObject.length - 1) {
-      nextImg = 0
+    prevExp = mainExp
+    mainExp = nextExp
+    if (nextExp >= experiences.length - 1) {
+      nextExp = 0
       isAtRightEnd = true
     } else {
-      nextImg++
+      nextExp++
       isAtLeftEnd = false
     }
     loadGallery()
@@ -165,14 +282,14 @@ function scrollRight() {
 
 function scrollLeft() {
   if (!isAtLeftEnd) {
-    nextImg = mainImg
-    mainImg = prevImg
+    nextExp = mainExp
+    mainExp = prevExp
 
-    if (prevImg === 0) {
-      prevImg = imgObject.length - 1
+    if (prevExp === 0) {
+      prevExp = experiences.length - 1
       isAtLeftEnd = true
     } else {
-      prevImg--
+      prevExp--
       isAtRightEnd = false
     }
     loadGallery()
