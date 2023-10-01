1df70a4f87a3dfb493ddab3f6eb83603176e77c0
Michele Montesi <michelemontesi36@gmail.com>
2023-10-01 17:53:34 +0200

    Made bubbles dinamic

--- scripts/bubblesScript.js
+++ scripts/bubblesScript.js
@@ -1,45 +1,118 @@
 var openModalBtns = document.querySelectorAll('.bubble')
 
-openModalBtns.forEach((bubble) => {
-  const randomDuration = Math.random() * (15 - 4) + 4
-  bubble.style.animationDuration = `${randomDuration}s`
-})
+var bubbleData = {
+  openModal1: {
+    itaTitle: 'Linguaggi di Programmazione',
+    engTitle: 'Programming Language',
+    itaList: [
+      'Python',
+      'Java',
+      'C',
+      'C#',
+      'Scripting Bash da terminale',
+      'Sviluppatore Web di base (HTML, CSS, JavaScript, PHP, MySQL)',
+      'Assembly x86',
+      'Database: MySQL (base)',
+      'Go/Golang',
+      'Sviluppo Android: Kotlin (Android Studio)',
+      'Conoscenza di base di programmazione con OpenMP, MPI',
+    ],
+    engList: [
+      'Python',
+      'Java',
+      'C',
+      'C#',
+      'Bash scripting from terminal',
+      'Web Developer base (HTML, CSS, JavaScript, PHP, MySQL)',
+      'Assembly x86',
+      'Database: MySQL (basic)',
+      'Go/Golang',
+      'Android Developing: Kotlin (Android Studio)',
+      'Basic programming knowledge with OpenMP, MPI',
+    ],
+  },
+  openModal2: {
+    itaTitle: 'Sistemi Operativi',
+    engTitle: 'Operating Systems',
+    itaList: ['Linux', 'Windows', 'MacOS', 'Android'],
+    engList: ['Linux', 'Windows', 'MacOS', 'Android'],
+  },
+  openModal3: {
+    itaTitle: 'Strumenti Informatici',
+    engTitle: 'IT Tools',
+    itaList: [
+      'Docker (Docker compose, Docker Swarm, Docker container)',
+      'Teleport',
+      'Ansible',
+      'Active Directory',
+      'Freeipa',
+    ],
+    engList: [
+      'Docker (Docker compose, Docker Swarm, Docker container)',
+      'Teleport',
+      'Ansible',
+      'Active Directory',
+      'Freeipa',
+    ],
+  },
+}
 
-openModalBtns.forEach(function (btn) {
-  btn.addEventListener('click', function () {
-    var modalId = this.getAttribute('id').replace('openM', 'm')
-    var modal = document.getElementById(modalId)
-    modal.classList.add('scroll-animation')
-    modal.style.display = 'flex'
+document
+  .getElementById('language')
+  .addEventListener('change', onLanguageChangeSkills)
+
+function onLanguageChangeSkills() {
+  currentLanguage = languageSelectorExp.value
+}
+
+function loadSkills() {
+  openModalBtns.forEach(function (btn) {
+    btn.addEventListener('click', function () {
+      let modal = document.getElementById('modalSkills')
+      let modalTitle = modal.querySelector('h2') // Selezione dell'elemento del titolo in italiano
+      let modalList = modal.querySelector('ul') // Selezione dell'elemento della lista in italiano
+
+      let bubbleId = this.getAttribute('id')
+      let data = bubbleData[bubbleId]
+
+      let skillsList = currentLanguage === 'ita' ? data.itaList : data.engList
+
+      console.log(currentLanguage)
+      modalTitle.textContent =
+        currentLanguage === 'ita' ? data.itaTitle : data.engTitle
+      modalList.innerHTML = skillsList
+        .map((item) => `<li><p>${item}</p></li>`)
+        .join('')
+
+      modal.style.display = 'flex'
+    })
   })
-})
+}
 
 function closeModal(modalId) {
-  var modal = document.getElementById(modalId)
+  let modal = document.getElementById(modalId)
   modal.style.display = 'none'
 }
 
-var closeBtns = document.querySelectorAll('.close')
-closeBtns.forEach(function (btn) {
-  btn.addEventListener('click', function () {
-    var modalId = this.classList[1].replace('close', 'modal')
-    closeModal(modalId)
-  })
+let closeBtn = document.querySelector('.close')
+closeBtn.addEventListener('click', function () {
+  let modal = document.getElementById('modalSkills')
+  modal.style.display = 'none'
 })
 
-var closeModalBG = document.querySelectorAll('.modal-container')
-closeModalBG.forEach(function (bg) {
-  bg.addEventListener('click', function (event) {
-    if (
-      !event.target.classList.contains('modal-content') &&
-      !event.target.classList.contains('modal-body') &&
-      event.target.tagName !== 'LI' &&
-      event.target.tagName !== 'UL' &&
-      event.target.tagName !== 'P' &&
-      event.target.tagName !== 'H2'
-    ) {
-      var modalId = this.getAttribute('id')
-      closeModal(modalId)
-    }
-  })
+let closeModalBG = document.querySelector('.modal-container')
+closeModalBG.addEventListener('click', function (event) {
+  if (
+    !event.target.classList.contains('modal-content') &&
+    !event.target.classList.contains('modal-body') &&
+    event.target.tagName !== 'LI' &&
+    event.target.tagName !== 'UL' &&
+    event.target.tagName !== 'P' &&
+    event.target.tagName !== 'H2'
+  ) {
+    let modalId = this.getAttribute('id')
+    closeModal(modalId)
+  }
 })
+
+loadSkills()
